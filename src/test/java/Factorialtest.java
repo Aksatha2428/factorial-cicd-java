5ef882e75934c0b3a1aee51f05c3dc836c9552e7
Aksatha <akshu19047@gmail.com>
2026-09-22 20:43:54 +0530

    Fix factorial test

--- src/test/java/Factorialtest.java
+++ src/test/java/Factorialtest.java
@@ -10,7 +10,7 @@ class FactorialTest {
 
     @Test
     void testFactorialOfFive() {
-        assertEquals(12, Factorial.calculate(5));
+        assertEquals(120, Factorial.calculate(5));
     }
 
     @Test
