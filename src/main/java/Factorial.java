ee86143c8fa62a08051654d78f9b5f7d8d58c112
Aksatha <akshu19047@gmail.com>
2026-09-22 20:30:36 +0530

    Update factorial implementation

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -5,7 +5,7 @@ public class Factorial {
         }
 
         long result = 1;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i <= n; i++) {
             result *= i;
         }
         return result;
